5e51f65a34603ba74066fac97f0b2b119eb0af59
Amit Kumar <amitnitsri25@gmail.com>
2020-11-08 17:30:27 +0530

    UC17 : Ability To Update The Contact Information In Database

--- src/test/java/com/Capgemini/AddressBook/AddressBookTest.java
+++ src/test/java/com/Capgemini/AddressBook/AddressBookTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 
 import org.junit.Test;
 
+import exception.AddressBookException;
 
 public class AddressBookTest {
 
@@ -13,6 +14,15 @@ public class AddressBookTest {
 		long entries = addressBook.readAddressBookData().size();
 		System.out.println("Number of entries:- " + entries);
 		Assert.assertEquals(1, entries);
+	}
+
+	@Test
+	public void givenContactName_WhenUpdated_ShouldSyncWithDB() throws AddressBookException {
+		AddressBook addressBook = new AddressBook();
+		addressBook.readAddressBookData();
+		addressBook.updateContact("Amit", "9149947504");
+		boolean ans = addressBook.checkAddressBookDataInSyncWithDB("Amit");
+		Assert.assertTrue(ans);
 	}
 
 }
